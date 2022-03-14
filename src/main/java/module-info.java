757afc77e2f610d2ecca007146e5e86700824091
Fabrizio Zeballos <54884274+FabrizioZeb@users.noreply.github.com>
2022-03-14 13:56:58 +0100

    Add files via upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.asa.asagui {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.asa.asagui to javafx.fxml;
+    exports com.asa.asagui;
+}
