a6cf0c2d70220f3a01d14e9653c0601ed4a3e03f
enekoreto <73554201+enekoreto@users.noreply.github.com>
2022-04-03 19:50:33 +0200

    dao

---
+++ src/org/is2/asa/view/AdopterWindow.java
@@ -0,0 +1,104 @@
+package org.is2.asa.view;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class AdopterWindow extends JPanel {
+	
+	public AdopterWindow() {
+
+	}
+
+	public void prepareWindow() {
+		// Declaracion de paneles
+		this.setLayout(new BorderLayout());
+		JPanel panel_sup = new JPanel((new BorderLayout()));
+		JPanel panel_inf = new JPanel();
+
+		/********* Seccion superior **********/
+
+		JPanel panel_sup0 = new JPanel(new BorderLayout());
+		JPanel panel_sup1 = new JPanel(new BorderLayout());
+
+		// panel superior
+		JButton home = new JButton("Home");
+		JButton refugios = new JButton("Refugios");
+		JButton adoptantes = new JButton("Adoptantes");
+		JButton voluntariado = new JButton("Voluntariado");
+		JButton FAQ = new JButton("FAQ");
+		JButton usuario = new JButton("Guille");
+		JButton imagen_usuario = new JButton(new ImageIcon("imagen.png"));
+		
+		JPanel x = new JPanel( new FlowLayout());
+		x.add(home);
+		x.add(refugios);
+		x.add(adoptantes);
+		x.add(voluntariado);
+		x.add(FAQ);
+		panel_sup0.add(x, BorderLayout.WEST);
+		
+		
+		JPanel y = new JPanel( new FlowLayout());
+		y.add(usuario);
+		y.add(imagen_usuario);
+		panel_sup0.add(y, BorderLayout.EAST);
+
+		panel_sup0.setBorder(BorderFactory.createLineBorder(Color.black));
+		panel_sup0.setVisible(true);
+
+		// panel inferior
+
+		JTextField busqueda = new JTextField("Buscar...");
+		busqueda.setPreferredSize(new Dimension(200, 20));
+		JButton buscar = new JButton("Buscar");
+
+		JPanel z = new JPanel(new FlowLayout());
+		z.add(busqueda);
+		z.add(buscar);
+		
+		panel_sup1.add(z, BorderLayout.WEST);
+		panel_sup1.add(new JButton("Filtros"), BorderLayout.EAST);
+		
+		panel_sup1.setBorder(BorderFactory.createLineBorder(Color.black));
+
+		panel_sup1.setVisible(true);
+
+		// añadimos paneles
+		panel_sup.add(panel_sup0, BorderLayout.NORTH);
+		panel_sup.add(panel_sup1, BorderLayout.SOUTH);
+
+		panel_sup.setVisible(true);
+
+		/********* Fin seccion superior ***********/
+
+		/********** SECCION INFERIOR ***********/
+		
+		JPanel panel_animales = new JPanel();
+		panel_animales.setLayout(new BoxLayout(panel_animales, BoxLayout.Y_AXIS));
+		
+		//añadimos animales
+		String[] nombres = {"Toby", "Marcelino", "Amadeo", "Juan", "Homosexual", "Eneko", "Antonio", "Fabri", "Javi", "Salgueiro"};
+		AnimalPanel[] animales = new AnimalPanel[nombres.length]; 
+		
+		for(int i =0; i < animales.length; i++) {
+			animales[i] = new AnimalPanel();
+			animales[i].prepareWindow(nombres[i]);
+			panel_animales.add(animales[i]);
+		}
+		
+		JScrollPane scroll = new JScrollPane();
+		scroll.setPreferredSize(new Dimension(500,300));
+		scroll.setViewportView(panel_animales);
+		
+		
+		/********** FIN SECCION INFERIOR *********/
+
+		// Añadimos paneles al principal
+
+		this.setBorder(BorderFactory.createLineBorder(Color.black));
+		this.add(panel_sup, BorderLayout.NORTH);
+		this.add(scroll, BorderLayout.CENTER);
+	}
+
+}
---
+++ src/org/is2/asa/view/AnimalPanel.java
@@ -0,0 +1,56 @@
+package org.is2.asa.view;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class AnimalPanel extends JPanel {
+
+	public AnimalPanel() {
+
+	}
+
+	public void prepareWindow(String nombre) {
+
+		this.setLayout(new FlowLayout());
+
+		JButton perrillo = new JButton(new ImageIcon("perrillo.png"));
+		this.add(perrillo, BorderLayout.EAST);
+
+		
+		JPanel panel_centro = new JPanel(new BorderLayout());
+		
+		Label nombre_perro = new Label(nombre);
+		Label descripcion = new Label(
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et magna a velit mollis sagittis at nec nisi. ");
+		
+		panel_centro.add(nombre_perro, BorderLayout.NORTH);
+		panel_centro.add(descripcion, BorderLayout.CENTER);
+		
+		this.add(panel_centro);
+		
+		
+		JButton adoptar = new JButton("Adoptar");
+		JButton info = new JButton("info");
+		this.add(info, FlowLayout.RIGHT);
+		this.add(adoptar, FlowLayout.RIGHT);
+		this.setBorder(BorderFactory.createLineBorder(Color.black));
+		this.setPreferredSize(new Dimension(600, 100));
+
+	}
+
+	public AnimalPanel(LayoutManager layout) {
+		super(layout);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AnimalPanel(boolean isDoubleBuffered) {
+		super(isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AnimalPanel(LayoutManager layout, boolean isDoubleBuffered) {
+		super(layout, isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+}
