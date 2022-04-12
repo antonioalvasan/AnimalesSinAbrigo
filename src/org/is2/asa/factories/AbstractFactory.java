22b21ca959fb6ff9ece6bb6e0050c13234024df1
Fabrizio Zeballos <54884274+FabrizioZeb@users.noreply.github.com>
2022-04-12 17:48:31 +0200

    factory 1.2

--- src/org/is2/asa/factories/AbstractFactory.java
+++ src/org/is2/asa/factories/AbstractFactory.java
@@ -1,8 +1,12 @@
 package org.is2.asa.factories;
 
+import org.is2.asa.model.Species;
 import org.json.JSONObject;
 
 public abstract class AbstractFactory <T>{
+
+    private Species species;
+    private String type;
 
     public T generateObject(JSONObject info){
         T newObject = createInstance(info);
